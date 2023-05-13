3e79c72df168cf0c4a3578e4bd5cfa300e060cf7
Urki Aristu <aristu.138167@e.unavarra.es>
2023-05-13 13:42:31 +0200

    Creando Coin e intentando test GetCoinController

---
+++ src/app/Infrastructure/Persistence/FileCoinDataSource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Infrastructure\Persistence;
+
+use App\Application\CoinDataSource\CoinDataSource;
+use App\Domain\Coin;
+use App\Domain\User;
+
+class FileCoinDataSource implements CoinDataSource
+{
+    public function findByName(string $email): ?Coin
+    {
+        return new Coin('1', "Bitcoin","B",12,1);
+    }
+
+    public function getAll(): array
+    {
+        return [new Coin('1', "Bitcoin","B",12,1), new Coin('2', "Ethereum","Eth",20,3)];
+    }
+}
